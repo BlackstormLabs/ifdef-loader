199a2b642134d76842fb30b00986cb4679e7cd72
yofreke <sparkyjoe94@aim.com>
2017-06-30 17:59:24 -0700

    add support for webpack 2 options object

--- ifdef-loader.ts
+++ ifdef-loader.ts
@@ -6,9 +6,16 @@ import { parse } from "./preprocessor";
 
 export = function (source: string, map) {
   this.cacheable && this.cacheable();
-  // ?json=... contains JSON encoded data object
-  const query = queryString.parse(url.parse(this.query).query);
-  const data = JSON.parse(query.json);
+  let data;
+  if (typeof this.query === 'string') {
+    // webpack 1
+    // ?json=... contains JSON encoded data object
+    const query = queryString.parse(url.parse(this.query).query);
+    data = JSON.parse(query.json);
+  } else {
+    // webpack 2
+    data = this.query;
+  }
 
   const verboseFlag = "ifdef-verbose";
   const verbose = data[verboseFlag];
@@ -31,4 +38,3 @@ export = function (source: string, map) {
     this.callback(new Error(errorMessage));
   }
 };
-
